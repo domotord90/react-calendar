b4713a5b5fc96ddab695695d0839b549b3ffec60
domotord90 <jazzmaster@freemail.com>
2019-11-16 17:20:05 +0100

    Updates

---
+++ precache-manifest.dc7ce13acf05da9020bc410e320bc315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f9067722b33ef494f213ed41de3202",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "c0d5cf05a8ef7f3957bb",
+    "url": "/react-calendar/static/css/main.959d7b24.chunk.css"
+  },
+  {
+    "revision": "6ef8644be1773b822e54",
+    "url": "/react-calendar/static/js/2.c4fa5ec1.chunk.js"
+  },
+  {
+    "revision": "c0d5cf05a8ef7f3957bb",
+    "url": "/react-calendar/static/js/main.8eb6e286.chunk.js"
+  },
+  {
+    "revision": "eea25540c698f005c30e",
+    "url": "/react-calendar/static/js/runtime-main.80169e3b.js"
+  }
+]);
